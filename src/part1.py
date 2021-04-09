4e2733e70249a0a8215cecac276d812f02cbb6e2
yjin910 <yoojin9160@gmail.com>
2021-04-09 07:14:36 -0700

    Add comments for part1

--- src/part1.py
+++ src/part1.py
@@ -2,10 +2,18 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, explode, split, avg, count, round, lower
 
 
-# read movies.csv and ratings.csv from the dataset_path and create a new dataset consists of users id with number of
-# movies and genre he/she watched.
-# return the dataset
 def search_users_by_id(dataset_path: str, users: list):
+    """
+    Searches the number of movies and genres that a given user or a group of users watched.
+
+    The number of movies watched are calculated by counting the number of movies that the user rated.
+    The number of genres watched are calculated by counting all the genres of the movie user rated.
+    The genres are counted without duplication by splitting each movie genres to individual rows.
+
+    :param dataset_path: The path of the dataset
+    :param users: The list of users
+    :return: The dataset of number of movies and genres that users watched
+    """
     # set local[*] to utilize all cores
     spark_session = SparkSession.builder.master("local[*]").appName("App").getOrCreate()
 
@@ -35,9 +43,17 @@ def search_users_by_id(dataset_path: str, users: list):
     return result.sort(result["userId"].cast("int").asc())
 
 
-# number of watches = count(rating), average of rating = avg(rating) from ratings.csv
-# average of rating is rounded to 2 decimal places
 def search_movie_by_id(dataset_path: str, n: int):
+    """
+    Searches the movie by id, calculate average rating and number of users watched the movie.
+
+    The average rating of the movie is calculated by averaging values of rating column which are related to the movie.
+    The number of users watched are calculated by counting all the users rated the movie.
+
+    :param dataset_path: The path of the dataset
+    :param n: The movie id
+    :return: The dataset of average rating and number of users watched the movie
+    """
     # set local[*] to utilize all cores
     spark_session = SparkSession.builder.master("local[*]").appName("App").getOrCreate()
 
@@ -47,6 +63,8 @@ def search_movie_by_id(dataset_path: str, n: int):
     # cast String type column 'rating' to double type for calculation
     ratings_dataset = ratings_dataset.withColumn("rating", ratings_dataset["rating"].cast("double"))
 
+    # calculate average rating and count number of watched users
+    # average of rating is rounded to 2 decimal places
     result = ratings_dataset.where(col("movieId") == n) \
         .join(movies_dataset, "movieId") \
         .groupBy("movieId", "title") \
@@ -55,10 +73,21 @@ def search_movie_by_id(dataset_path: str, n: int):
     return result
 
 
-# all the movies containing the given string are searched
-# number of watches = count(rating), average of rating = avg(rating) from ratings.csv
-# average of rating is rounded to 2 decimal places
 def search_movie_by_title(dataset_path: str, title: str):
+    """
+    Searches the movie by title, calculate average rating and number of users watched the movie.
+
+    The movie will be searched by checking if the movie title contains the given value.
+    The average rating of the movie is calculated by averaging values of rating column which are related to the movie.
+    The number of users watched are calculated by counting all the users rated the movie.
+
+    All the movies containing the given title are searched together.
+    e.g., if user searched "toy", the result will contain "Toy Story", "Babes is Toyland", etc.
+
+    :param dataset_path: The path of the dataset
+    :param title: The title of the movie
+    :return: The dataset of average rating and number of users watched the movie
+    """
     # set local[*] to utilize all cores
     spark_session = SparkSession.builder.master("local[*]").appName("App").getOrCreate()
 
@@ -68,20 +97,30 @@ def search_movie_by_title(dataset_path: str, title: str):
     # cast String type column 'rating' to double type for calculation
     ratings_dataset = ratings_dataset.withColumn("rating", ratings_dataset["rating"].cast("double"))
 
+    # filter the dataset by checking if a movie title contains the given value
     filtered_movies_dataset = movies_dataset.filter(lower(movies_dataset.title).like("%" + title.lower() + "%"))
     filtered_movies_ratings_dataset = filtered_movies_dataset.join(ratings_dataset, "movieId")
 
+    # calculate average rating and count number of watched users
+    # average of rating is rounded to 2 decimal places
     result = filtered_movies_ratings_dataset.groupBy("movieId", "title") \
         .agg(round(avg("rating"), 2).alias("avgRating"), count("rating").alias("numWatched"))
 
     return result.sort(result["movieId"].cast("int").asc())
 
 
-# TODO: commit needs to change
-# all the movies containing the given string are searched
-# number of watches = count(rating), average of rating = avg(rating) from ratings.csv
-# average of rating is rounded to 2 decimal places
 def search_genre(dataset_path: str, genres: list):
+    """
+    Searches movies by given list of genres
+
+    The functionality searches for each genre and stores the result data of the genre in separate datasets.
+    The all movies in that genre are searched by checking if the given genre is contained in genres column of the movie.
+
+    :param dataset_path: The path of the dataset
+    :param genres: The list of genres
+    :return: The list that contains the list containing name of genres and
+             the list containing datasets of each genres
+    """
     # set local[*] to utilize all cores
     spark_session = SparkSession.builder.master("local[*]").appName("App").getOrCreate()
 
@@ -95,10 +134,12 @@ def search_genre(dataset_path: str, genres: list):
     # filter to get data of given genres
     filtered_genre_dataset = split_genre_dataset.filter(lower(col("genres")).isin(genres_lowercase))
 
+    # split dataset to datasets of each genre
     list_of_genres = filtered_genre_dataset.select("genres").distinct().rdd.flatMap(lambda x: x).collect()
     genre_datasets_array = [filtered_genre_dataset.where(filtered_genre_dataset["genres"] == genre) for genre in
                             list_of_genres]
 
+    # remove genres column in each dataset
     for i in range(len(genre_datasets_array)):
         genre_datasets_array[i] = genre_datasets_array[i].drop("genres")
 
@@ -107,28 +148,40 @@ def search_genre(dataset_path: str, genres: list):
     return result
 
 
-# all the movies containing the given string are searched
-# number of watches = count(rating), average of rating = avg(rating) from ratings.csv
-# average of rating is rounded to 2 decimal places
 def search_movie_by_year(dataset_path: str, year: str):
+    """
+    Searches movies by year.
+
+    The movies related to the year are searched by checking if the given year is contained in title column.
+
+    :param dataset_path: The path of the dataset
+    :param year: The year
+    :return: The dataset of movies related to the given year
+    """
     # set local[*] to utilize all cores
     spark_session = SparkSession.builder.master("local[*]").appName("App").getOrCreate()
 
     movies_dataset = spark_session.read.options(header='True').csv(dataset_path + "movies.csv")
 
+    # filter the dataset to get movies related to the given year
+    # average of rating is rounded to 2 decimal places
     result = movies_dataset \
         .filter(lower(movies_dataset.title).like("%(" + year + ")%")) \
         .select("movieId", "title")
-    # result = filtered_movies_by_year_dataset.groupBy("movieId", "title")
 
     return result.sort(result["movieId"].asc())
 
 
-# read movies.csv and ratings.csv from the dataset_path and create a new dataset consists of movie names with highest
-# rating.
-# movie rating is calculated by adding all ratings from ratings.csv.
-# return the dataset
 def list_movies_by_rating(dataset_path: str, n: int):
+    """
+    Lists the movies by rating.
+
+    The movie rating is calculated by adding all ratings of the movie.
+
+    :param dataset_path: The path of the dataset
+    :param n: The number of columns to return, top n rated movies
+    :return: The dataset of the top rated movies
+    """
     # set local[*] to utilize all cores
     spark_session = SparkSession.builder.master("local[*]").appName("App").getOrCreate()
 
@@ -150,6 +203,15 @@ def list_movies_by_rating(dataset_path: str, n: int):
 
 # number of watches = count(movieId) from ratings.csv
 def list_movies_by_watches(dataset_path: str, n: int):
+    """
+    Lists the movies by number of users watched.
+
+    The number of users watched the movie is calculated by counting the number of users that rated the movie.
+
+    :param dataset_path: The path of the dataset
+    :param n: The number of columns to return, top n rated movies
+    :return: The dataset of the top rated movies
+    """
     # set local[*] to utilize all cores
     spark_session = SparkSession.builder.master("local[*]").appName("App").getOrCreate()
 
